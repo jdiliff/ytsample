e3e6e9e6d2ee17f3256554b89edc52fcd409d6b4
Jessica Iliff <jdiliff@jessicas-mbp-2.home>
2019-01-29 01:50:13 -0600

    d

--- script.js
+++ script.js
@@ -15,12 +15,22 @@ $(function() {
 
             request.execute(function(response) {
                 var results = response.result; 
+                $('#results').html('');
                 $.each(results.items, function(index, item) {
+                    $.get("tpl/item.html", function(data) {
+                        $('#results').append(tplawesome(data, [{'title':item.snippet.title, "videoid":item.id.videId}]));
+                    });
                     console.log(item); 
                 });
+                resetVideoHeight();
             });
         });
+        $(window).on('resize', resetVideoHeight); 
     }); 
+
+    function resetVideoHeight() {
+        $(".video").css("height", $('#results').width() * 9/16); 
+    }
 
 function init() {
     gapi.client.setApiKey("AIzaSyB8LA4BQojhhjwpGFhSFEYQrJHdC1PXiYI");
